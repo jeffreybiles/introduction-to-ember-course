a51afeff0da93bdc5077a5929b5dd048c664c5ef
jeffreybiles <bilesjeffrey@gmail.com>
2014-06-29 10:10:21 -0500

    ember is getting data from express

--- express/server.js
+++ express/server.js
@@ -1,12 +1,26 @@
 var express = require('express');
 var app = express();
-
+var _ = require('lodash');
 var Datastore = require('nedb')
   , db = new Datastore({ filename: 'database', autoload: true });
 
+var insertMonster = function(params){
+  db.find({}, function(err, docs){
+    var last_id
+    if(docs.length == 0){
+      last_id = 0
+    } else {
+      docs = _.sortBy(docs, 'id')
+      last_id = docs[docs.length - 1].id
+    }
+    params['id'] = last_id + 1
+    db.insert(params)
+  })
+}
+
 app.get('/api/monsters', function(req, res){
   db.find({}, function(err, docs){
-    res.send(docs);
+    res.send({monsters: docs});
   });
 });
 
